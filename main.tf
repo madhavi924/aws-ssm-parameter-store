c00815e00971a688e3ce9e52e04f01a9c3f7e5c5
madhavi <madhavilatha.008@gmail.com>
2023-05-11 15:39:36 -0500

    ansible

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
   type  = "String"
   value = "Hello World"
+  type        = "SecureString"
+  key_id = "9b7293fb-063e-408d-acf3-b8d3ee6b1e99"
 }
